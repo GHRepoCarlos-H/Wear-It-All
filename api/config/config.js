c8f186139397ec99b5518975f0a587a2243ab56a
Car Hez <redbluecanary@outlook.com>
2024-02-20 18:31:48 -0800

    update

--- api/config/config.js
+++ api/config/config.js
@@ -17,7 +17,8 @@ module.exports = {
                 username: "wear_it_again_database_user",
                 password: "8jJ1erSdc894RqSJyFyLQXnbyKL8akCJ",
                 database: "wear_it_again_database",
-                host: "dpg-cnae61ol5elc7396l3s0-a ",
+                host: "dpg-cnae61ol5elc7396l3s0-a",
                 dialect: "postgres",
+            
         },
 };
