9ce8b75ebe781ab7789e9c1c57b849db89b73585
Carlos Hernandez <130628984+GHRepoCarlos-H@users.noreply.github.com>
2024-02-22 16:35:16 -0800

    Update config.js

--- api/config/config.js
+++ api/config/config.js
@@ -1,28 +1,4 @@
 //folder config file config.js
-module.exports = {
-        development: {
-                username: "postgres",
-                password: "E3aDd25De4gf5BebcGgDA4C*aDg3Ed5b",
-                database: "railway",
-                hostname: "viaduct.proxy.rlwy.net",
-                dialect: "postgres",
-            },
-        test: {
-                username: "postgres",
-                password: "E3aDd25De4gf5BebcGgDA4C*aDg3Ed5b",
-                database: "railway",
-                hostname: "viaduct.proxy.rlwy.net",
-                dialect: "postgres",
-        },
-        production: {
-                username: "postgres",
-                password: "E3aDd25De4gf5BebcGgDA4C*aDg3Ed5b",
-                database: "railway",
-                hostname: "viaduct.proxy.rlwy.net",
-                dialect: "postgres",
-            
-        },
-};
 
 
 
